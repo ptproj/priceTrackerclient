2f8229fca946d76faf0336b9d82ec4b4d79b3889
212628143@mby.co.il <212628143@mby.co.il>
2022-01-26 18:39:47 +0200

    models

---
+++ src/models/classcompany.ts
@@ -0,0 +1,10 @@
+export class Company{
+    id:number | undefined ;
+    name:string | undefined;
+    password:string | undefined;
+    companylink:string | undefined;
+    packageid?:number;
+    startofsubscription?:Date;
+    constructor()
+    {}
+  }
