6fab874c56315c17831f043483f337f672706da9
323777862@mby.co.il <323777862@mby.co.il>
2022-01-30 22:22:00 +0200

    login

--- src/app/login/costumerlogin/costumerlogin.component.ts
+++ src/app/login/costumerlogin/costumerlogin.component.ts
@@ -21,15 +21,19 @@ export class CostumerloginComponent implements OnInit {
   {
 }
  loginForm:FormGroup=new FormGroup({
-    email:new FormControl("",[Validators.required,Validators.email]),
-    password:new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(20)]),
+    "email":new FormControl("",[Validators.required,Validators.email]),
+   "password":new FormControl("",[Validators.required,Validators.minLength(5),Validators.maxLength(20)]),
   })
   saveuser(){
-    this.costumer=new Costumer("323777862@mby.co.il","thisisit")
-    //this.costumer=new Costumer(this.loginForm.controls.email.value,this.loginForm.controls.password.value)
-    this.costumer=new Costumer(  this. loginForm.value[0],  this. loginForm.value[1])  
-    alert( this. loginForm.value[0])
-    alert(   this. loginForm.value[1])
+   // this.costumer=new Costumer("323777862@mby.co.il","thisisit")
+  //  this.costumer=new Costumer(this.loginForm.controls.['email'].value,this.loginForm.controls.password.value)
+    this.costumer=new Costumer( this. loginForm.get("email")?.value, this. loginForm.get("password")?.value)  
+    var x=this.loginForm.value[0]
+    //alert(x)
+    alert( this. loginForm.get("email")?.value)
+    
+   // alert( this.loginForm.value)
+    
    // this.loginservice.postcostumer(this.costumer).subscribe(data=>alert(data));
 
   }
