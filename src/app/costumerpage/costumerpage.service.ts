0812c036def315b22fea6e7d5a6c15e4fa42637b
faigy b <323777862@PC5-29>
2022-02-16 14:03:06 +0200

    m

--- src/app/costumerpage/costumerpage.service.ts
+++ src/app/costumerpage/costumerpage.service.ts
@@ -3,40 +3,60 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Costumerproduct } from "src/models/classcostumerproduct";
 import { HttpHeaders } from "@angular/common/http";
+
 @Injectable({
     providedIn: 'root'
   })
-
+  
   export class CostumerpageService {
-    constructor(private http:HttpClient) { }
+    
    
      
-    token:any=sessionStorage.getItem('token');
     
     
-    httpOptions={
-
-       headers:new HttpHeaders({
-           'Authorization':this.token.tostring()
-       })
-   }
+    products:Costumerproduct[] | undefined
+  
     
-   
+  constructor(private http:HttpClient) { 
+     
+  }
    
    
   
- getcostumerproduct():Observable<Costumerproduct[]>{
+  getcostumerproduct():Observable<Costumerproduct[]>{
+    const token:any=sessionStorage.getItem('token');
+    const httpOptions={
+  
+         headers:new HttpHeaders({
+             'Authorization':token.tostring()
+         })
+     }
+    
    const costumerid=sessionStorage.getItem('costumerid');
    const c=Number(costumerid)
-   return  this.http.get<Costumerproduct[]>("api/Customerproduct/"+c,this.httpOptions)
+   return  this.http.get<Costumerproduct[]>("api/Customerproduct/"+c,httpOptions)
      
  }
 deletecostumerproduct(productid:number){
-    this.token.tostring();
-    this.http.get<Costumerproduct[]>("api/Customerproduct/"+productid,this.httpOptions)
+  const token:any=sessionStorage.getItem('token');
+    const httpOptions={
+  
+         headers:new HttpHeaders({
+             'Authorization':token.tostring()
+         })
+     }
+    
+    this.http.get<Costumerproduct[]>("api/Customerproduct/"+productid,httpOptions)
 }
 addcostumerproduct(costumerproduct:Costumerproduct):Observable<Costumerproduct>{
-    return  this.http.post<Costumerproduct>("api/Customerproduct/",costumerproduct,this.httpOptions);
+  const token:any=sessionStorage.getItem('token');
+    const httpOptions={
+  
+         headers:new HttpHeaders({
+             'Authorization':token.tostring()
+         })
+     }
+    return  this.http.post<Costumerproduct>("api/Customerproduct/",costumerproduct,httpOptions);
 }
   }
   
